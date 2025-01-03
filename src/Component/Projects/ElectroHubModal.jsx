65909e922e288d0dd15666aef8603d984d43faae
Mohaiminul375 <mohaiminul375@gmail.com>
2025-01-03 20:17:40 +0600

    add new project

---
+++ src/Component/Projects/ElectroHubModal.jsx
@@ -0,0 +1,76 @@
+import { FaXmark } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
+const ElectroHubModal = () => {
+  return (
+    <div className="modal-box md:max-w-2xl bg-[#09101A] text-white font-inter p-14">
+      <div className="flex justify-end">
+        <form method="dialog">
+          {/* if there is a button in form, it will close the modal */}
+          <button className="p-1 rounded-full border-2 border-red-700 text-red-500">
+            <FaXmark />
+          </button>
+        </form>
+      </div>
+      <h2 className="text-[#55E6A5] text-2xl font-bold">Electro-Hub</h2>
+      <h5 className="font-bold mb-5">
+        A Full functionality Gadget E-commerce Website.
+      </h5>
+      <hr className="border-b-2 border-[#55E6A5]" />
+      <ul className="mt-5 space-y-2 list-disc">
+        <li>
+          Role-based access ensures secure management for users and admins.
+          Users can purchase gadgets while admins oversee operations seamlessly.
+        </li>
+        <li>
+          Users enjoy a smooth shopping experience with SSLCommerz payments.
+          They can track their order progress with detailed updates
+        </li>
+        <li>
+          Admins have full control to add, edit, or delete products. They can
+          manage orders by approving, shipping, etc.
+        </li>
+        <li>
+          Comprehensive user and order management ensures efficiency. Admins can
+          monitor activities while users track orders effortlessly.
+        </li>
+      </ul>
+      <div className="mt-2">
+        <h3>
+          {" "}
+          <span className="font-bold text-[#55E6A5]">Uses Technology:</span>
+          <span>
+            {" "}
+            HTML, Tailwind CSS, NextUI, Next.Js, Typescript, Next Auth,
+            Express.js, Mongo DB.
+          </span>
+        </h3>
+        <div className="mt-4 flex items-center justify-between">
+          <Link
+            target="_blank"
+            to="https://electro-hub-tau.vercel.app"
+            className="border-2 border-[#55E6A5] p-2 rounded-full text-sm hover:bg-[#55E6A5] hover:font-bold hover:text-black"
+          >
+            Live Links
+          </Link>
+          <Link
+            target="_blank"
+            to="https://github.com/mohaiminul375/electro-hub"
+            className="border-2 border-[#55E6A5] p-2 rounded-full text-sm hover:bg-[#55E6A5] hover:font-bold hover:text-black"
+          >
+            Github Client
+          </Link>
+          <Link
+            target="_blank"
+            to="https://github.com/mohaiminul375/electro-server"
+            className="border-2 border-[#55E6A5] p-2 rounded-full text-sm hover:bg-[#55E6A5] hover:font-bold hover:text-black"
+          >
+            Github Server
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ElectroHubModal;
--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 import merit_matrix from "../../assets/merit-matrix-project.png";
 import group_guru from "../../assets/group_guru.png";
 import nihongo_dojo from "../../assets/nihongo-dojo.png";
+import electro_hub from "../../assets/electro-hub.png";
 import scas from "../../assets/scas.png";
 import { Zoom } from "react-awesome-reveal";
 import MeritMatrixModal from "./MeritMatrixModal";
 import GroupGuruModal from "./GroupGuruModal";
 import ScacModal from "./ScacModal";
 import Nihongo from "./Nihongo";
+import ElectroHubModal from "./ElectroHubModal";
 const Projects = () => {
   return (
     <div className="mt-36 px-10 md:px-0" id="projects">
@@ -18,20 +20,21 @@ const Projects = () => {
         {/* project-1 */}
         <Zoom triggerOnce="false" duration={2000}>
           <div className="group relative items-center justify-center overflow-hidden cursor-pointer md:w-96">
-            <div className="w-full rounded-md ">
+            <div className="w-full rounded-md">
               <img
                 className="w-full rounded-lg group-hover:scale-105 transition-transform duration-1000"
-                src={merit_matrix}
+                src={electro_hub}
                 alt=""
               />
             </div>
-            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-[#55E6A5]/90 group-hover:via-[#55E6A5]/30 group-hover:to-[#55E6A5]/90"></div>
+            {/* Default image visibility with minimal dark on hover */}
+            <div className="absolute inset-0 bg-[#09101A]/30 group-hover:bg-[#09101A]/60 transition-all duration-500"></div>
             <div className="absolute inset-0 flex flex-col items-center justify-center px-9 text-center translate-y-[55%] space-y-1 group-hover:translate-y-0 transition-all">
-              <h3 className="text-2xl text-[#55E6A5] group-hover:text-white">
-                Merit-Matrix
+              <h3 className="text-2xl text-black bg-[#55E6A5] group-hover:bg-transparent group-hover:text-white p-2 rounded-md duration-300 transition-all">
+                Electro-Hub
               </h3>
               <p className="text-lg opacity-0 group-hover:opacity-100 transition-opacity">
-                A scholarship Management Website
+                A Full functionality Gadget E-commerce Website
               </p>
               <h3
                 onClick={() =>
@@ -43,29 +46,31 @@ const Projects = () => {
               </h3>
             </div>
             {/* modal */}
-
             <dialog id="my_modal_1" className="modal">
-              <MeritMatrixModal></MeritMatrixModal>
+              <ElectroHubModal></ElectroHubModal>
             </dialog>
           </div>
         </Zoom>
+
         {/* project-2 */}
+
         <Zoom triggerOnce="false" duration={2000}>
           <div className="group relative items-center justify-center overflow-hidden cursor-pointer md:w-96">
-            <div className="w-full rounded-md ">
+            <div className="w-full rounded-md">
               <img
                 className="w-full rounded-lg group-hover:scale-105 transition-transform duration-1000"
-                src={group_guru}
+                src={merit_matrix}
                 alt=""
               />
             </div>
-            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-[#55E6A5]/90 group-hover:via-[#55E6A5]/30 group-hover:to-[#55E6A5]/90"></div>
+            {/* Default image visibility with minimal dark on hover */}
+            <div className="absolute inset-0 bg-[#09101A]/30 group-hover:bg-[#09101A]/60 transition-all duration-500"></div>
             <div className="absolute inset-0 flex flex-col items-center justify-center px-9 text-center translate-y-[55%] space-y-1 group-hover:translate-y-0 transition-all">
-              <h3 className="text-2xl text-[#55E6A5] group-hover:text-white">
-                Group-Guru
+              <h3 className="text-2xl text-black bg-[#55E6A5] group-hover:bg-transparent group-hover:text-white p-2 rounded-md duration-300 transition-all">
+                Merit-Matrix
               </h3>
               <p className="text-lg opacity-0 group-hover:opacity-100 transition-opacity">
-                An Online group study assignment platform
+                A scholarship Management Website
               </p>
               <h3
                 onClick={() =>
@@ -78,60 +83,29 @@ const Projects = () => {
             </div>
             {/* modal */}
             <dialog id="my_modal_2" className="modal">
-              <GroupGuruModal></GroupGuruModal>
+              <MeritMatrixModal></MeritMatrixModal>
             </dialog>
           </div>
         </Zoom>
-        {/* project-3 */}
+
+        {/* project-2 */}
         <Zoom triggerOnce="false" duration={2000}>
           <div className="group relative items-center justify-center overflow-hidden cursor-pointer md:w-96">
-            <div className="w-full rounded-md ">
+            <div className="w-full rounded-md">
               <img
                 className="w-full rounded-lg group-hover:scale-105 transition-transform duration-1000"
                 src={nihongo_dojo}
                 alt=""
               />
             </div>
-            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-[#55E6A5]/90 group-hover:via-[#55E6A5]/30 group-hover:to-[#55E6A5]/90"></div>
+            {/* Default image visibility with minimal dark on hover */}
+            <div className="absolute inset-0 bg-[#09101A]/30 group-hover:bg-[#09101A]/80 transition-all duration-500"></div>
             <div className="absolute inset-0 flex flex-col items-center justify-center px-9 text-center translate-y-[55%] space-y-1 group-hover:translate-y-0 transition-all">
-              <h3 className="text-2xl text-[#55E6A5] group-hover:text-white">
-                Nihonog Dojo
+              <h3 className="text-2xl text-black bg-[#55E6A5] group-hover:bg-transparent group-hover:text-white p-2 rounded-md duration-300 transition-all">
+                Nihongo Dojo
               </h3>
               <p className="text-lg opacity-0 group-hover:opacity-100 transition-opacity">
                 A Simple Japanese language learning Platform
-              </p>
-              <h3
-                onClick={() =>
-                  document.getElementById("my_modal_3").showModal()
-                }
-                className="opacity-0 group-hover:opacity-100 transition-opacity hover:underline hover:font-bold"
-              >
-                View Details
-              </h3>
-            </div>
-            {/* modal */}
-            <dialog id="my_modal_3" className="modal">
-              <Nihongo />
-            </dialog>
-          </div>
-        </Zoom>
-        {/* project-4 */}
-        <Zoom triggerOnce="false" duration={2000}>
-          <div className="group relative items-center justify-center overflow-hidden cursor-pointer md:w-96">
-            <div className="w-full rounded-md ">
-              <img
-                className="w-full rounded-lg group-hover:scale-105 transition-transform duration-1000"
-                src={scas}
-                alt=""
-              />
-            </div>
-            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-[#55E6A5]/90 group-hover:via-[#55E6A5]/30 group-hover:to-[#55E6A5]/90"></div>
-            <div className="absolute inset-0 flex flex-col items-center justify-center px-9 text-center translate-y-[55%] space-y-1 group-hover:translate-y-0 transition-all">
-              <h3 className="text-2xl text-[#55E6A5] group-hover:text-white">
-                Safia's Creative art studio
-              </h3>
-              <p className="text-lg opacity-0 group-hover:opacity-100 transition-opacity">
-                An Arts & Crafts showcase Website.
               </p>
               <h3
                 onClick={() =>
@@ -144,8 +118,78 @@ const Projects = () => {
             </div>
             {/* modal */}
             <dialog id="my_modal_4" className="modal">
-              <ScacModal></ScacModal>
+              <Nihongo />
             </dialog>
+          </div>
+        </Zoom>
+
+        {/* project-3 */}
+        <Zoom triggerOnce="false" duration={2000}>
+          <div className="group relative items-center justify-center overflow-hidden cursor-pointer md:w-96">
+            <div className="w-full rounded-md">
+              <img
+                className="w-full rounded-lg group-hover:scale-105 transition-transform duration-1000"
+                src={group_guru}
+                alt=""
+              />
+            </div>
+            {/* Default image visibility with minimal dark on hover */}
+            <div className="absolute inset-0 bg-[#09101A]/30 group-hover:bg-[#09101A]/80 transition-all duration-500"></div>
+            <div className="absolute inset-0 flex flex-col items-center justify-center px-9 text-center translate-y-[55%] space-y-1 group-hover:translate-y-0 transition-all">
+              <h3 className="text-2xl text-black bg-[#55E6A5] group-hover:bg-transparent group-hover:text-white p-2 rounded-md duration-300 transition-all">
+                Group-Guru
+              </h3>
+
+              <p className="text-lg opacity-0 group-hover:opacity-100 transition-opacity">
+                An Online group study assignment platform
+              </p>
+              <h3
+                onClick={() =>
+                  document.getElementById("my_modal_3").showModal()
+                }
+                className="opacity-0 group-hover:opacity-100 transition-opacity hover:underline hover:font-bold"
+              >
+                View Details
+              </h3>
+            </div>
+            {/* modal */}
+            <dialog id="my_modal_3" className="modal">
+              <GroupGuruModal></GroupGuruModal>
+            </dialog>
+          </div>
+        </Zoom>
+
+        {/* project-5 */}
+        <Zoom triggerOnce="false" duration={2000}>
+          <div className="group relative items-center justify-center overflow-hidden cursor-pointer md:w-96">
+            {/* <div className="w-full rounded-md ">
+              <img
+                className="w-full rounded-lg group-hover:scale-105 transition-transform duration-1000"
+                src={scas}
+                alt=""
+              />
+            </div> */}
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-[#55E6A5]/90 group-hover:via-[#55E6A5]/30 group-hover:to-[#55E6A5]/90"></div>
+            {/* <div className="absolute inset-0 flex flex-col items-center justify-center px-9 text-center translate-y-[55%] space-y-1 group-hover:translate-y-0 transition-all">
+              <h3 className="text-2xl text-[#55E6A5] group-hover:text-white">
+                Safia's Creative art studio
+              </h3>
+              <p className="text-lg opacity-0 group-hover:opacity-100 transition-opacity">
+                An Arts & Crafts showcase Website.
+              </p>
+              <h3
+                onClick={() =>
+                  document.getElementById("my_modal_5").showModal()
+                }
+                className="opacity-0 group-hover:opacity-100 transition-opacity hover:underline hover:font-bold"
+              >
+                View Details
+              </h3>
+            </div> */}
+            {/* modal */}
+            {/* <dialog id="my_modal_4" className="modal">
+              <ScacModal></ScacModal>
+            </dialog> */}
           </div>
         </Zoom>
       </div>
