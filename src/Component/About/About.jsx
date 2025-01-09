ad35ba6aab8ffd2b68a81994d7a846baa4dc8bfd
Mohaiminul375 <mohaiminul375@gmail.com>
2025-01-09 23:26:17 +0600

    update about section

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -8,41 +8,48 @@ const About = () => {
         About Me
       </div>
       <div className="mt-10 flex flex-col md:flex-row justify-between items-center">
-    
         <div className="md:w-1/2 px-8 md:px-0">
-        <Slide direction="right" triggerOnce={true} duration={2000}>
-          <h2 className="text-3xl mb-3">Hi There,</h2>
-          <h5 className="text-xl">
-            This Mohaiminul Islam. Front-end Web Developer React with a strong passion
-            for coding. My ultimate aspiration is to evolve into a Full Stack Web Development proficient in the MERN stack. Driven and results-oriented, I am eager to contribute
-            and demonstrate my skills through customized solutions, and
-            validated projects, I am Always ready to prove myself through
-            dedicated and impactful work
-          </h5>
-          <div className="mt-10 grid md:grid-cols-2 gap-5">
-            <div>
-              <h2 className="text-2xl font-bold mb-2">Name:</h2>
-              <p className="text-xl">Mohaiminul Islam</p>
+          <Slide direction="right" triggerOnce={true} duration={2000}>
+            <h2 className="text-3xl mb-3">Hi There,</h2>
+            <h5 className="text-xl">
+              This Mohaiminul Islam. Front-End Web Developer React & Next.Js
+              with a strong passion for coding. my long-term goal is to become a
+              Full Stack Web Developer, specializing in the MERN stack. Though I
+              don't come from an IT background, my enthusiasm drives me to
+              continuously learn and improve, providing the best services to my
+              clients. I also have experience in social work through past
+              volunteer roles, where I developed strong interpersonal skills
+              that continue to benefit my professional journey.
+            </h5>
+            <h5 className="text-xl mt-5">
+              For me, coding isn't just a career it's a passion, and I’m
+              dedicated to making a positive impact both in the tech world and
+              my community..
+            </h5>
+            <div className="mt-10 grid md:grid-cols-2 gap-5">
+              <div>
+                <h2 className="text-2xl font-bold mb-2">Name:</h2>
+                <p className="text-xl">Mohaiminul Islam</p>
+              </div>
+              <div>
+                <h2 className="text-2xl font-bold mb-2">Email:</h2>
+                <p className="text-xl">mohaiminul375@gmail.com</p>
+              </div>
+              <div>
+                <h2 className="text-2xl font-bold mb-2">Phone:</h2>
+                <p className="text-xl">+8801533057483</p>
+              </div>
+              <div>
+                <h2 className="text-2xl font-bold mb-2">GitHub:</h2>
+                <p className="text-base">https://github.com/mohaiminul375</p>
+              </div>
             </div>
-            <div>
-              <h2 className="text-2xl font-bold mb-2">Email:</h2>
-              <p className="text-xl">mohaiminul375@gmail.com</p>
-            </div>
-            <div>
-              <h2 className="text-2xl font-bold mb-2">Phone:</h2>
-              <p className="text-xl">+8801533057483</p>
-            </div>
-            <div>
-              <h2 className="text-2xl font-bold mb-2">GitHub:</h2>
-              <p className="text-base">https://github.com/mohaiminul375</p>
-            </div>
-          </div>
           </Slide>
         </div>
-        
+
         <div className="mt-5 md:mt-0 md:w-1/2 flex justify-center">
-        <Slide direction="left" triggerOnce={true} duration={2000}>
-          <img src={mern} alt="" />
+          <Slide direction="left" triggerOnce={true} duration={2000}>
+            <img src={mern} alt="" />
           </Slide>
         </div>
       </div>
